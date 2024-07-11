04246dbdbeca195ee1cef005fbbd8037aeda647d
Mark Kvetny <m.kvetny@outlook.com>
2024-07-11 12:47:11 +0200

    add parrot ai completion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,4 +7,28 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"frankroeder/parrot.nvim",
+		tag = "v0.3.2",
+		keys = {
+			{
+				"<leader>ai",
+				function()
+					require("parrot").setup({
+						providers = {
+							openai = {
+								api_key = { "op read 'op://dev/OpenAI/credential'" },
+							},
+							anthropic = {
+								api_key = { "op read 'op://dev/Anthropic/credential'" },
+							},
+						},
+					})
+					print("Parrot loaded!")
+				end,
+				desc = "Load Parrot",
+			},
+		},
+		dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+	},
 }
