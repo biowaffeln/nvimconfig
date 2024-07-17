921ac784190ca4781dfd35086f978a1ef254e436
Mark Kvetny <m.kvetny@outlook.com>
2024-07-17 21:20:56 +0200

    add parrot shortcuts

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -20,6 +20,23 @@ return {
 					},
 				},
 			})
+
+			local function map(key, cmd, desc)
+				vim.keymap.set("n", key, "<cmd>Prt" .. cmd .. "<CR>", { desc = desc })
+			end
+
+			local function vmap(key, cmd, desc)
+				map(key, cmd, desc)
+				vim.keymap.set("v", key, ":'<,'>Prt" .. cmd .. "<CR>", { desc = desc })
+			end
+
+			vmap("<leader>k", "Rewrite", "Rewrite code with instructions")
+			vmap("<leader>i", "Implement", "Complete code")
+			vmap("<leader>l", "ChatNew", "Open new Chat")
+
+			map("<leader>pt", "ChatToggle", "[P]arrot: [T]oggle chat")
+			map("<leader>pr", "ChatRespond", "[P]arrot: [R]espond in chat")
+			map("<leader>ps", "Stop", "[P]arrot: [S]top generation")
 		end,
 		dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
 	},
