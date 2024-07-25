52a52691bbe532453e5a77d60749e8f442cc1ccb
Mark Kvetny <m.kvetny@outlook.com>
2024-07-25 15:23:03 +0200

    add new highlighting autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -21,3 +21,26 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 	end,
 })
+
+-- toggle cursorline on insert mode
+vim.api.nvim_create_augroup("CursorLineToggle", { clear = true })
+vim.api.nvim_create_autocmd("InsertEnter", {
+	group = "CursorLineToggle",
+	callback = function()
+		vim.opt.cursorline = true
+	end,
+})
+vim.api.nvim_create_autocmd("InsertLeave", {
+	group = "CursorLineToggle",
+	callback = function()
+		vim.opt.cursorline = false
+	end,
+})
+
+-- highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+	end,
+})
